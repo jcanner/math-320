8db3144288998624e0b8522196ff11f8054422c6
Judith Canner <jcanner@csumb.edu>
2026-09-16 12:27:25 -0700

    clt

---
+++ note-outlines/question-types.R
@@ -0,0 +1,61 @@
+# Question Types
+
+:::{.callout-tip collapse="true"}
+#### Answer
+discrete
+:::
+  
+  
+  ```{r}
+#| label: wrap-2
+#| echo: false
+checkdown::check_question(answer = "standard deviation",
+                          options = c("average",
+                                      "expectation",
+                                      "expected value",
+                                      "mean.",
+                                      "standard deviation"),
+                          button_label = "Check Your Answer", 
+                          type = "radio")
+```
+
+
+```{r}
+#| label: wrap-3-a
+#| echo: false
+checkdown::check_question(answer = "True",
+                          options = c("True",
+                                      "False"),
+                          type = "select",
+                          button_label = "Check Your Answer")
+```
+
+
+
+```{webr}
+#| label: wrap-8-b
+#| exercise: ex_wrap_8_b
+#| caption: "Use R to find the Probability"
+pnorm(   , mean =  , sd =   )
+```
+
+
+::: {.solution exercise="ex_wrap_8_b"}
+
+##### Solution
+
+```{webr}
+#| exercise: ex_wrap_8_b
+#| solution: true
+#| echo: false
+pnorm(0.5, mean = 1, sd = 0.2)
+```
+
+:::
+  
+  ```{webr}
+#| exercise: ex_wrap_8_b
+#| check: true
+#| echo: false
+gradethis::grade_this_code()
+```
